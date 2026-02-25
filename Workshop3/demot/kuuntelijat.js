dea12516584ab78a60127e86da9b274823d5056a
Taru Laine <taru.s.laine@student.laurea.fi>
2026-02-25 21:35:06 +0200

    kolmonen

---
+++ Workshop3/demot/kuuntelijat.js
@@ -0,0 +1,54 @@
+//HARJOITUS 1
+
+// Etsi eka nappi
+let ekaNappi = document.querySelector("#ekaNappi");
+// Lisää nappiin kuuntelija
+ekaNappi.addEventListener("click", function () {
+  alert("Klikkasit minua!");
+});
+
+
+function showTable() {
+  //muuttujat
+  const nimi = "Jaana";
+  const tehtava = "koodaaja";
+
+  //taulukko template stringillä
+  const html =`
+    <table border="1">
+      <thead>
+        <tr><th>Nimi</th><th>Tehtävä</th></tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>${nimi}</td>
+          <td>${tehtava}</td>
+        </tr>
+      </tbody>
+    </table>
+  `;
+  // taulukko oikeaan kohtaan
+  document.querySelector("#taulukonPaikka").innerHTML = html;
+}
+
+// Etsi toka nappi
+let tokaNappi = document.querySelector("#tokaNappi");
+// Lisää nappiin kuuntelija
+tokaNappi.addEventListener("click", function() { showTable();
+});
+
+// HARJOITUS 2
+
+// Etsi toka otsikko
+let tokaOtsikko = document.querySelector("#tokaOtsikko");
+// Lisää otsikkoon kuuntelija ja mouseover alert
+tokaOtsikko.addEventListener("mouseover", function () {
+  alert("Stepped over me with a mouse!");
+});
+
+// Etsi kenttä sivulta
+let kentta = document.querySelector("input");
+// Lisää kuuntelija
+kentta.addEventListener("focus", function () {
+  alert("Aktivoit kentän");
+});

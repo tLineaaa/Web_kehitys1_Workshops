b7a7ab7c4d0b6e1e2b2908235a9080a3fec733ff
Taru Laine <taru.s.laine@student.laurea.fi>
2026-03-05 17:05:26 +0200

    kolmonenmelkeinvalmis

--- Workshop3/demot/kuuntelijat.js
+++ Workshop3/demot/kuuntelijat.js
@@ -55,9 +55,48 @@ ekaOtsikko.addEventListener("click", function () {
 ekaOtsikko.style.color = "red";
 });
 
-// Etsi kenttä sivulta
-let kentta = document.querySelector("input");
-// Lisää kuuntelija
-kentta.addEventListener("focus", function () {
-  alert("Aktivoit kentän");
+
+// HARJOITUS 3
+
+let paikka = document.querySelector("#status");
+let tekstikentta = document.querySelector("textarea");
+let lomake = document.querySelector("#feedbackForm");
+let merkkimaara = document.querySelector("#charcount");
+const maxpituus = 200; //maksimipituus
+
+//lisätään kuuntelija focus, kun tekstikenttä aktivoituu, näkyville tulee teksti ja väri vaihtuu
+tekstikentta.addEventListener("focus", function () {
+  paikka.textContent = "Aktivoit kentän";
+  paikka.style.backgroundColor = "lightgreen";
+})
+
+// sama kuin yllä, mutta nyt blur, kun tekstikentästä lähdetään ja näkyville tyhjää, väri pois
+tekstikentta.addEventListener("blur", function() {
+  paikka.textContent = "";
+  tekstikentta.style.backgroundColor = "white";
 });
+
+// merkkimäärän tarkistus
+tekstikentta.addEventListener("input", function(){
+  let nytpituus = tekstikentta.value.length; //tsekkaa pituus
+  charcount.textContent = `${nytpituus}/${maxpituus}` //näytä pituus / max pituus
+
+//jos merkkimäärä ylittyy DEAKTIVOINTI UUPUU
+  if (nytpituus > maxpituus) {
+    tekstikentta.style.backgroundColor = "red" //tekstikentän väri muuttuu punaiseksi
+    merkkimaara.style.color = "red" // merkkimäärä muuttuu punaiseksi
+  }
+})
+
+// keydown
+
+// submit-kuuntelija
+lomake.addEventListener("submit", function(esto) { 
+  esto.preventDefault(); //esto oikealle lähetykselle
+  if (tekstikentta.value === "") { //jos tekstikenttä on tyhjä, hälytä
+    alert("Et voi lähettää tyhjää palautetta");
+    return
+  }
+  alert("Palaute tallennettu"); //muutoin näytä tämä viesti (pop-up)
+  tekstikentta.value = ""; // tyhjennä tekstikenttä
+})
